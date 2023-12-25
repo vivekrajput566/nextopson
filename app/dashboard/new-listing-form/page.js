2b003b75557d47d67fd882434d999b6b4813c1fb
vivekrajput566 <rajputvivek566@gmail.com>
2023-12-25 23:01:48 +0530

    updating files

---
+++ app/dashboard/new-listing-form/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropertyForm from '../new-property-listing'
+
+function NewListingForm() {
+  return (
+    <>
+    
+    <PropertyForm />
+    
+    </>
+
+  )
+}
+
+export default NewListingForm
