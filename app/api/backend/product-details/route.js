b227fea42668b8303f4f8c1da54e410ee6f25f72
vivekrajput566 <rajputvivek566@gmail.com>
2024-01-20 23:32:08 +0530

    updating files

--- app/api/backend/product-details/route.js
+++ app/api/backend/product-details/route.js
@@ -22,7 +22,7 @@ export async function POST(req , res) {
     
     await mongoose.connect(connectionString)
     const productData = await Propertylisting.findOne({productId:productId}).select('-mobileno');
-    const productPhotos = await Propertyphotos.find({productId:productId}).select('filename');
+    const productPhotos = await Propertyphotos.find({productId:productId}).select('fileName');
     
     //console.log(!productData)
     if(!productData){
