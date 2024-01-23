e22bebc62625001dbc6d056f07fc6d4419c1b06d
vivekrajput566 <rajputvivek566@gmail.com>
2024-01-23 13:43:34 +0530

    updating files

--- app/dashboard/new-property-listing.js
+++ app/dashboard/new-property-listing.js
@@ -387,7 +387,7 @@ const PropertyForm = () => {
 
 
         <div className="upload-photos-body flex items-center justify-center w-full  sm:p-1 sm:p-2 lg:p-10">
-      <label htmlFor="dropzone-file" className="flex flex-col items-center w-full  justify-center  h-52 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+      <label htmlFor="dropzone-file" className="flex flex-col items-center w-full bg-[#374151] justify-center  h-52 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer   ">
         <div className="flex flex-col items-center justify-center pt-5 pb-6 ">
           <svg className="w-10 h-10 mb-4 text-white " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
