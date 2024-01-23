6c9b139af33a6e6c49403a26c23bf1337ec0bc2f
vivekrajput566 <rajputvivek566@gmail.com>
2024-01-23 15:32:05 +0530

    updating files

--- app/api/backend/listNewProperty/saveListing/route.js
+++ app/api/backend/listNewProperty/saveListing/route.js
@@ -10,7 +10,7 @@ import AWS from 'aws-sdk';
 import { getServerSession } from "next-auth";
 import { AuthOptions } from "../../../authOptions";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-
+import sharp from 'sharp';
 
 export async function POST(req, res) {
 
@@ -74,13 +74,15 @@ export async function POST(req, res) {
           const byteData = await file.arrayBuffer();
           const buffer = Buffer.from(byteData);
           
-
+          const webBuffer = await sharp(buffer)
+          .webp({ quality: 80 }) // Adjust quality as needed
+          .toBuffer();
 
     
           const response = await client_s3.send(new PutObjectCommand({
             Bucket: process.env.AWS_BUCKET,
             Key: `productPhotos/${fileName}.webp`, // Use .webp extension
-            Body: buffer,
+            Body: webBuffer,
             ContentType: 'image/webp', // Set correct MIME type
           }));
           console.log(response);
